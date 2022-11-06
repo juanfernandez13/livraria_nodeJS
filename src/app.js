1688ed5cee404339eea0725a0c3b2a1355e918dc
Juan Pablo <juanp1310@outlook.com>
2022-11-06 13:01:13 -0300

    feat: route get - busca por id e todos os livros

--- src/app.js
+++ src/app.js
@@ -4,4 +4,24 @@ const app = express();
 
 app.use(express.json());
 
+const livros = [
+    {id: 1, 'titulo':'Senhor dos Aneis'},
+    {id: 2, 'titulo':'O Hobbit'}
+];
+
+app.get('/', (req,res) =>{ 
+    res.status(200).send('Curso de node');
+});
+
+app.get('/livros', (req,res) => {
+    res.status(200).json(livros)
+});
+
+app.get('/livros/:id', (req,res) => {
+    const index = buscarLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+
+
 export default app;
