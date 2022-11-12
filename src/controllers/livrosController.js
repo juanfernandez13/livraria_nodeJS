684349505e034409fb9da0b919d59da6b8bd9259
Juan Pablo <juanp1310@outlook.com>
2022-11-12 19:06:33 -0300

    feat: implementando o método POST

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,9 +1,20 @@
-import livros from "./models/Livro.js"
+import livros from "../models/Livro.js"
 
 class LivroController {
     static listarLivros = (req,res) => {
         livros.find((err, livros) =>{
             res.status(200).json(livros)
+        })
+    }
+
+    static cadastrarLivro = (req,res) => {
+        const livro = new livros(req.body);
+        livro.save((err) => {
+            if(err){
+                res.status(500).send({message: `${err.message} - falha ao cadastrar o livro`});
+            } 
+
+            res.status(201).send(livro.toJSON()); 
         })
     }
 }
