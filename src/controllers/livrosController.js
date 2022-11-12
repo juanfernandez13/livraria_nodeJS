3faa9ac132a4d85fa2a536d3aacb0978cf8c7a43
Juan Pablo <juanp1310@outlook.com>
2022-11-12 18:05:18 -0300

    feat: implementando o método GET

---
+++ src/controllers/livrosController.js
@@ -0,0 +1,11 @@
+import livros from "./models/Livro.js"
+
+class LivroController {
+    static listarLivros = (req,res) => {
+        livros.find((err, livros) =>{
+            res.status(200).json(livros)
+        })
+    }
+}
+
+export default LivroController;
