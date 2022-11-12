370cadf938a71ede2e5b40ae3af8608fb82aae10
Juan Pablo <juanp1310@outlook.com>
2022-11-12 19:08:48 -0300

    refactor: arquitetura MVC

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import livros from "./models/Livro.js"
+import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => {
@@ -10,19 +11,7 @@ db.once("open", () => {
 const app = express();
 
 app.use(express.json());
-
-// const livros = [
-//     {id: 1, 'titulo':'Senhor dos Aneis'},
-//     {id: 2, 'titulo':'O Hobbit'}
-// ];
-
-app.get('/', (req,res) =>{ 
-    res.status(200).send('Curso de node');
-});
-
-app.get('/livros', (req,res) => {
-    res.status(200).json(livros)
-});
+routes(app);
 
 app.get('/livros/:id', (req,res) => {
     const index = buscarLivro(req.params.id);
