24f0c4e4a6e6d942455350fee36b83ee05f4337d
Juan Pablo <juanp1310@outlook.com>
2022-11-14 10:44:15 -0300

    refactor: arquitetura MVC

--- src/app.js
+++ src/app.js
@@ -13,31 +13,4 @@ const app = express();
 app.use(express.json());
 routes(app);
 
-app.get('/livros/:id', (req,res) => {
-    const index = buscarLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
-
-app.post('/livros', (req,res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro foi cadastrado com sucesso')
-});
-
-app.put('/livros/:id', (req,res) => {
-    const index = buscarLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(201).send('Livro foi alterado com sucesso')
-});
-
-app.delete('/livros/:id', (req,res) => {
-    const {id} = req.params;
-    const index = buscarLivro(id);
-    livros.splice(index,1)
-    res.status(201).send(`Livro ${id} removido com sucesso`)
-});
-
-const buscarLivro = (id) =>{
-    return livros.findIndex(livro => livro.id == id);
-};
-
 export default app;
